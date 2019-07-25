98b8a518cee6e4dac0b286b9dac173ae1e00fd42
stephentsoy92 <alanstephentsoy@gmail.com>
2019-07-25 13:06:26 -0400

    functionality works

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -3,42 +3,86 @@ create an app that has various buttons on the top of the different animal kingdo
 */
 
 // 1. create the animal array
-var topics = ["dog", "cat", "zebra", "lion", "bull", "eagle", "cobra", "shark", "cheetah", "tarantula"]
+var topics = ["pasta", "car", "cellphone", "hat", "drake", "angelina jolie", "michelle obama", "shoe", "blue", "budha"];
 
 // 2. make the variables necessary
 
 
 // 3. display array on the DOM
-for (var i=0; i<topics.length; i++)
+function topicList ()
 {
-    $(".animal-btn-arr").append("<button topic="+topics[i]+">"+topics[i]+"</button>")
-}
+    $(".animal-btn-arr").empty();
+    for (var i=0; i<topics.length; i++)
+    {
+        $(".animal-btn-arr").append('<button class="topic-btn btn btn-primary" topic="'+topics[i]+'">'+topics[i]+'</button>');
+    };
+};
+// Calling the renderButtons function to display the intial buttons
+topicList();
 
 //4. function for when the user click the buttons the we call the funtion to obtain the api for the giphys
-$("button").on("click",function ()
-    {
-        //if I wanted to caputer the innerText of $(this) how would i do it?
-        var chosenTopic = $(this).attr("topic")
+function pushGif ()
+{
+    $(".topic-btn").on("click",function ()
+        {
+            console.log(this);
+            //if I wanted to caputer the innerText of $(this) how would i do it?
+            var chosenTopic = $(this).attr("topic");
 
-        console.log(chosenTopic)// WORKS to capture the button of the topic click
+            console.log(chosenTopic);// WORKS to capture the button of the topic click
 
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ chosenTopic+"&api_key=TXpTp5lyJYpyVcndS4tIlmnGPeFaKnGn&limit=10";
+            var queryURL = "https://api.giphy.com/v1/gifs/search?q="+ chosenTopic+"&api_key=TXpTp5lyJYpyVcndS4tIlmnGPeFaKnGn&limit=10";
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function(response) {
-        console.log(response.data)
-        var results= response.data
-        //for loop to go through the reponses array
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function(response) {
+            console.log(response.data)
+            var results= response.data
+            //for loop to go through the reponses array
 
-        for (var j=0; j<results.length;j++)
-        var gifRows = $("<img>")
-        images
-        $(".gifs").html('<img src="'+response.data[j].images.original.url+'">')
-        console.log(response.data[0].images.original.url)
-        console.log(response.data[0].images.original_still.url)
-        $
+            $(".gifs").empty();
+            for (var j=0; j<results.length;j++)
 
-    });
-})
+            // Display images and gifs on the DOM
+            $(".gifs").append('<img src="'+response.data[j].images.original.url+'"class="gif" data-still="'+response.data[0].images.original_still.url+'" data-animate="'+response.data[0].images.original.url+'" data-state="animate">');
+            
+            console.log(response.data[0].images.original.url)
+            console.log(response.data[0].images.original_still.url)
+
+            // 6. pausing or playing gifs 
+
+            $(".gif").on("click", function() 
+            {
+                var state = $(this).attr("data-state");
+                console.log(state)
+                if (state === "still") 
+                {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+                } 
+                else 
+                {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+                }
+            });
+
+        });
+    })
+}
+
+// Calling the pushGif function to display the gifs
+pushGif()
+
+// 5. add new topics
+function addTopic (){
+    var newTopic = $(".input-arr").val();
+    console.log(newTopic); // WORKS it captures the users input
+    topics.push(newTopic);
+    topicList()
+    pushGif()
+};
+// handles the submit button when the user clicks
+$(".submit-btn-arr").on("click", addTopic)
+
